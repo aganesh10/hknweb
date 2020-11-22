1aefdd185f42939bbfe0109a6c9a6c020d318f18
Ashwin Ganesh <16601451+aganesh10@users.noreply.github.com>
2020-11-21 20:41:39 -0500

    Pulled from upstream

---
+++ hknweb/authentication.py
@@ -0,0 +1,11 @@
+#More general authenticatioon features, get the roles
+#Connecting with CAS, LDAP
+
+#Under Academics/front end - course survey realted, login button
+
+1. Get user id and logout link, user role
+2. Persist all this in a django session
+
+def getUserID():
+    
+def createSession():
